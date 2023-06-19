7bb395e8add6da4e100372e3084ca497ea04a220
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-19 03:12:54 +0300

    feat: add a result alias

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
 use thiserror::Error;
 
+/// A type alias for [`Result<T, [`RSocketError>`]`].
+pub type RSocketResult<T> = Result<T, RSocketError>;
+
 /// A type to represent all possible errors that can occur when using this
 /// library.
 #[derive(Debug, Error)]
