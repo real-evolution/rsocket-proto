17035c5c17270f70be1df0e4acbbd3c45a36b2d4
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-19 03:13:09 +0300

    feat: add an error variant for invalid flags detection

--- src/error.rs
+++ src/error.rs
@@ -6,4 +6,7 @@ pub type RSocketResult<T> = Result<T, RSocketError>;
 /// A type to represent all possible errors that can occur when using this
 /// library.
 #[derive(Debug, Error)]
-pub enum RSocketError {}
+pub enum RSocketError {
+    #[error("invalid flags value: 0x{0:02X}")]
+    InvalidFlags(u16)
+}
