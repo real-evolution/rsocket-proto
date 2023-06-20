d774d9bacd677f1beac3121112b8eb8e58757c08
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-21 01:47:02 +0300

    ref: make `Frame<'_>`'s fields module-only visible

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -10,12 +10,37 @@ use crate::error::{RSocketError, RSocketResult};
 
 #[derive(Debug)]
 pub struct Frame<'a> {
-    pub header: FrameHeader,
-    pub body: FrameBody<'a>,
-    pub raw: &'a [u8],
+    pub(super) header: FrameHeader,
+    pub(super) body: FrameBody<'a>,
+    pub(super) raw: &'a [u8],
 }
 
 impl<'a> Frame<'a> {
+    #[inline(always)]
+    pub fn stream_id(&self) -> u32 {
+        self.header.stream_id
+    }
+
+    #[inline(always)]
+    pub fn frame_type(&self) -> FrameType {
+        self.header.frame_type
+    }
+
+    #[inline(always)]
+    pub fn flags(&self) -> Flags {
+        self.header.flags
+    }
+
+    #[inline(always)]
+    pub fn body(&self) -> &FrameBody<'a> {
+        &self.body
+    }
+
+    #[inline(always)]
+    pub fn raw(&self) -> &'a [u8] {
+        self.raw
+    }
+
     pub fn decode(input: &'a [u8]) -> RSocketResult<Self> {
         let (rem, header) = FrameHeader::decode(input)?;
         let (rem, body) = match header.frame_type {
