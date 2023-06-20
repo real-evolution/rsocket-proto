9b83bea453c0e5de9b0d6a9ed5eabf5e62737e24
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-20 22:12:54 +0300

    feat: add error variants for invalid flags & stream ids

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use thiserror::Error;
 
+use crate::frame::Flags;
+
 /// A type alias for [`Result<T, [`RSocketError>`]`].
 pub type RSocketResult<T> = Result<T, RSocketError>;
 
@@ -7,6 +9,12 @@ pub type RSocketResult<T> = Result<T, RSocketError>;
 /// library.
 #[derive(Debug, Error)]
 pub enum RSocketError {
-    #[error("invalid flags value: 0x{0:02X}")]
-    InvalidFlags(u16)
+    #[error("invalid stream identifier: expected 0x{expected:08X}, got 0x{actual:08X}")]
+    InvalidStreamId { expected: u32, actual: u32 },
+
+    #[error("invalid flags value: 0x{flags:04X}, mask = 0x{mask:04X}")]
+    InvalidFlags { flags: Flags, mask: Flags },
+
+    #[error("unexpected flag value: {flag:?}, expected = {expected_value}")]
+    UnexpectedFlagValue { flag: Flags, expected_value: bool },
 }
