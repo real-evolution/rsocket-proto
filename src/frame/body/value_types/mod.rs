59eec22afc0ea384034e78b2895d9e133bd67e59
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-22 18:36:13 +0300

    feat: implement a wrapper type for big-endian numbers

--- src/frame/body/value_types/mod.rs
+++ src/frame/body/value_types/mod.rs
@@ -1,9 +1,11 @@
+mod metadata;
+mod mime;
+mod num;
 mod resume_token;
 mod version;
-mod mime;
-mod metadata;
 
+pub use metadata::{Metadata, PrefixedMetadata, RestMetadata};
+pub use mime::MimeType;
+pub use num::{NonZero, NumTraits, Number};
 pub use resume_token::ResumeToken;
 pub use version::Version;
-pub use mime::MimeType;
-pub use metadata::{Metadata, PrefixedMetadata, RestMetadata};
---
+++ src/frame/body/value_types/num.rs
@@ -0,0 +1,64 @@
+use derive_more::Deref;
+use nom::combinator::verify;
+
+pub type NonZero<T> = Number<T, true>;
+
+#[derive(Debug, Clone, Deref)]
+pub struct Number<T, const ALLOW_ZERO: bool>(T);
+
+pub trait NumTraits: Sized {
+    fn zero() -> Self;
+    fn min() -> Self;
+    fn max() -> Self;
+}
+
+macro_rules! impl_unit {
+    ($t:ty: dec => $dec:ident, enc => $($enc:tt)*) => {
+        impl NumTraits for $t {
+            fn zero() -> Self {
+                Default::default()
+            }
+
+            fn min() -> Self {
+                Self::min_value()
+            }
+
+            fn max() -> Self {
+                Self::max_value()
+            }
+        }
+
+        impl<const ALLOW_ZERO: bool> Number<$t, ALLOW_ZERO> {
+            #[allow(unused)]
+            pub(crate) fn decode(input: &[u8]) -> nom::IResult<&[u8], Self> {
+                use nom::number::complete::$dec;
+
+                let (r, value) = if ALLOW_ZERO {
+                    $dec(input)?
+                } else {
+                    verify($dec, |&v| v != <$t>::zero())(input)?
+                };
+
+                Ok((r, Self(value)))
+            }
+
+            #[allow(unused)]
+            pub(crate) fn encode<'b, W: std::io::Write>(
+                &self,
+                writer: &'b mut W,
+            ) -> std::io::Result<&'b mut W> {
+                use byteorder::{WriteBytesExt, BE};
+
+                writer.$($enc)*(self.0)?;
+
+                Ok(writer)
+            }
+        }
+    };
+}
+
+impl_unit!(u8: dec => be_u8, enc => write_u8);
+impl_unit!(u16: dec => be_u16, enc => write_u16::<BE>);
+impl_unit!(u32: dec => be_u32, enc => write_u32::<BE>);
+impl_unit!(u64: dec => be_u64, enc => write_u64::<BE>);
+impl_unit!(u128: dec => be_u128, enc => write_u128::<BE>);
