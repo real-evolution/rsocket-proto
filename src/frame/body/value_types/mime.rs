ca5a20cbc887ba1f1c7f99d369a4c12d9b2c2af6
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-22 03:49:29 +0300

    feat: implement a wrapper type for mime type strings

---
+++ src/frame/body/value_types/mime.rs
@@ -0,0 +1,32 @@
+use derive_more::Deref;
+
+#[derive(Debug, Deref)]
+#[repr(transparent)]
+pub struct MimeType<'a>(&'a str);
+
+impl<'a> MimeType<'a> {
+    pub(crate) fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self> {
+        use nom::combinator::map_res;
+        use nom::multi::length_data;
+        use nom::number::complete::be_u8;
+
+        let (r, mime) =
+            map_res(length_data(be_u8), std::str::from_utf8)(input)?;
+
+        Ok((r, Self(mime)))
+    }
+
+    pub(crate) fn encode<'b, W: std::io::Write>(
+        &self,
+        writer: &'b mut W,
+    ) -> std::io::Result<&'b mut W> {
+        use byteorder::WriteBytesExt;
+
+        let buf = self.0.as_bytes();
+
+        writer.write_u8(buf.len() as u8)?;
+        writer.write_all(buf)?;
+
+        Ok(writer)
+    }
+}
--- src/frame/body/value_types/mod.rs
+++ src/frame/body/value_types/mod.rs
@@ -1,5 +1,7 @@
 mod resume_token;
 mod version;
+mod mime;
 
 pub use resume_token::ResumeToken;
 pub use version::Version;
+pub use mime::MimeType;
