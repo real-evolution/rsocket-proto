3882ab1e753152a5502a318acce9b11ca11cd0aa
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-23 20:20:33 +0300

    feat: add traits to enable abstracted decoding

--- src/frame/codec/mod.rs
+++ src/frame/codec/mod.rs
@@ -3,7 +3,16 @@ mod num;
 mod sequence;
 mod util;
 
+pub(crate) use chained::*;
 pub(crate) use context::*;
 pub(crate) use num::*;
 pub(crate) use sequence::*;
 pub(crate) use util::*;
+
+pub(crate) trait Decodable<'a>: Sized {
+    fn decode(input: &'a [u8]) -> nom::IResult<&'a [u8], Self>;
+}
+
+pub(crate) trait ContextDecodable<'a, C>: Sized {
+    fn decode_with(input: &'a [u8], cx: C) -> nom::IResult<&'a [u8], Self>;
+}
