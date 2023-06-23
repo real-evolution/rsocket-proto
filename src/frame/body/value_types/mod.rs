a9f53fb1014b18014c8ea951ad6f029d3109f879
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-23 20:22:12 +0300

    feat: implement a wrapper for error codes

--- src/frame/body/value_types/mod.rs
+++ src/frame/body/value_types/mod.rs
@@ -1,13 +1,15 @@
+mod data;
+mod error_code;
 mod metadata;
 mod mime;
 mod num;
 mod resume_token;
 mod version;
-mod data;
 
+pub use data::Data;
+pub use error_code::ErrorCode;
 pub use metadata::{Metadata, PrefixedMetadata, RestMetadata};
 pub use mime::MimeType;
 pub use num::{NonZero, NumTraits, Number};
 pub use resume_token::ResumeToken;
 pub use version::Version;
-pub use data::Data;
