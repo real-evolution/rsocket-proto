9718ee5d578c3a4a4827bb9c2c44ae3382f4abce
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-03 00:24:32 +0300

    doc: document `FrameHeader`

--- src/frame2/header/mod.rs
+++ src/frame2/header/mod.rs
@@ -4,21 +4,26 @@ mod r#type;
 pub use flags::Flags;
 pub use r#type::FrameType;
 
+/// A type to represent an rsocket frame header.
 #[derive(Debug, Clone, Copy, recode::Recode)]
+#[recode(decoder(error = "crate::Error"), encoder(error = "crate::Error"))]
 pub struct FrameHeader {
     stream_id: u32,
     type_flags: u16,
 }
 
 impl FrameHeader {
+    /// Gets frame stream identifier.
     pub const fn stream_id(&self) -> u32 {
         self.stream_id
     }
 
+    /// Gets frame type.
     pub const fn frame_type(&self) -> FrameType {
         FrameType::from_base_type((self.type_flags >> 10) as u8)
     }
 
+    /// Gets frame flags.
     pub const fn flags(&self) -> Flags {
         Flags::from_bits_truncate(self.type_flags % 0x03FF)
     }
