8b608b7028f0f33969d19e21a6a1a3a83a79c6c9
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-02 22:50:34 +0300

    feat: implement `Buf[Mut]` for `BufferWrapper<B>`

--- src/frame2/buffer.rs
+++ src/frame2/buffer.rs
@@ -1,9 +1,10 @@
+use recode::bytes;
 use std::ops::Deref;
 
 use super::FrameHeader;
 
-pub type Buffer = BufferWrapper<recode::bytes::Bytes>;
-pub type BufferMut = BufferWrapper<recode::bytes::BytesMut>;
+pub type Buffer = BufferWrapper<bytes::Bytes>;
+pub type BufferMut = BufferWrapper<bytes::BytesMut>;
 
 #[derive(Debug, Clone)]
 pub struct BufferWrapper<B> {
@@ -18,6 +19,64 @@ impl<B> BufferWrapper<B> {
 
     pub fn header(&self) -> &FrameHeader {
         &self.header
+    }
+}
+
+impl bytes::Buf for Buffer {
+    #[inline]
+    fn remaining(&self) -> usize {
+        self.inner.remaining()
+    }
+
+    #[inline]
+    fn chunk(&self) -> &[u8] {
+        self.inner.chunk()
+    }
+
+    #[inline]
+    fn advance(&mut self, cnt: usize) {
+        self.inner.advance(cnt)
+    }
+
+    #[inline]
+    fn copy_to_bytes(&mut self, len: usize) -> bytes::Bytes {
+        self.inner.copy_to_bytes(len)
+    }
+}
+
+unsafe impl bytes::BufMut for BufferMut {
+    #[inline]
+    fn remaining_mut(&self) -> usize {
+        self.inner.remaining_mut()
+    }
+
+    #[inline]
+    unsafe fn advance_mut(&mut self, cnt: usize) {
+        self.inner.advance_mut(cnt)
+    }
+
+    #[inline]
+    fn chunk_mut(&mut self) -> &mut bytes::buf::UninitSlice {
+        self.inner.chunk_mut()
+    }
+
+    #[inline]
+    fn put<T>(&mut self, src: T)
+    where
+        Self: Sized,
+        T: bytes::Buf,
+    {
+        self.inner.put(src)
+    }
+
+    #[inline]
+    fn put_slice(&mut self, src: &[u8]) {
+        self.inner.put_slice(src)
+    }
+
+    #[inline]
+    fn put_bytes(&mut self, val: u8, cnt: usize) {
+        self.inner.put_bytes(val, cnt)
     }
 }
 
