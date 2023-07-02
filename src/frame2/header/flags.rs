574f13e012691a931aabacbe1c269aa3b0aeefc3
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-02 19:19:23 +0300

    ref!: remove unnecessary conversion between `u16` and `Flags`

--- src/frame2/header/flags.rs
+++ src/frame2/header/flags.rs
@@ -1,4 +1,6 @@
-bitflags::bitflags! {
+use bitflags::bitflags;
+
+bitflags! {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub struct Flags: u16 {
         const NEXT = 1 << 5;
@@ -9,17 +11,5 @@ bitflags::bitflags! {
         const LEASE = Self::COMPLETE.bits();
         const RESUME = Self::FOLLOW.bits();
         const RESPOND = Self::FOLLOW.bits();
-    }
-}
-
-impl From<u16> for Flags {
-    fn from(bits: u16) -> Self {
-        Self::from_bits_truncate(bits)
-    }
-}
-
-impl From<Flags> for u16 {
-    fn from(flags: Flags) -> Self {
-        flags.bits()
     }
 }
