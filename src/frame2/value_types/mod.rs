4c71eaac8c72ed69526fb6989d1b5598b8c108f2
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-02 23:08:13 +0300

    feat: add remaining common value types as aliases to `recode` codecs

--- src/frame2/value_types/mod.rs
+++ src/frame2/value_types/mod.rs
@@ -3,3 +3,9 @@ mod version;
 
 pub use metadata::Metadata;
 pub use version::Version;
+
+use recode::codec;
+
+pub type Data = codec::UnprefixedBuffer;
+pub type MimeType = codec::Ascii<u8>;
+pub type ResumeToken = codec::Buffer<u16>;
