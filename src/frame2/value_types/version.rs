a6e5ae517568712a13048b4f5c61d2f83268bf24
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-02 23:50:19 +0300

    ref: specifiy encoding/decoding error types for `Version`

--- src/frame2/value_types/version.rs
+++ src/frame2/value_types/version.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, Copy, Clone, Eq, PartialEq, recode::Recode)]
+#[recode(decoder(error = "crate::Error"), encoder(error = "crate::Error"))]
 pub struct Version {
     pub major: u16,
     pub minor: u16,
