f3054e2c4a65fd202634151b34474333b8656433
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-03 00:26:11 +0300

    feat: implement `{De,En}coder` for `Frame`

--- src/frame2/mod.rs
+++ src/frame2/mod.rs
@@ -3,11 +3,13 @@ mod header;
 mod value_types;
 mod variant;
 
-use recode::bytes::Bytes;
 pub use buffer::*;
 pub use header::*;
 pub use value_types::*;
 pub use variant::*;
+
+use recode::bytes::{Bytes, BytesMut};
+use recode::{util::EncoderExt, Decoder, Encoder};
 
 #[derive(Debug)]
 pub struct Frame {
@@ -24,5 +26,34 @@ impl Frame {
     #[inline]
     pub fn variant(&self) -> &FrameVariant {
         &self.variant
+    }
+}
+
+impl Decoder<Bytes> for Frame {
+    type Error = crate::Error;
+
+    fn decode(buf: &mut Bytes) -> Result<Self, Self::Error> {
+        let mut header = FrameHeader::decode(buf)?;
+        let mut buf_wrapper = Buffer::new(buf.clone(), header);
+        let variant = FrameVariant::decode(&mut buf_wrapper)?;
+
+        (*buf, header) = buf_wrapper.into_parts();
+
+        Ok(Self { header, variant })
+    }
+}
+
+impl Encoder<BytesMut> for Frame {
+    type Error = crate::Error;
+
+    fn encode(item: &Self, buf: &mut BytesMut) -> Result<(), Self::Error> {
+        let mut buf_wrapper = BufferMut::new(buf.split(), item.header);
+
+        item.header.encode_to(&mut buf_wrapper)?;
+        item.variant.encode_to(&mut buf_wrapper)?;
+
+        (*buf, _) = buf_wrapper.into_parts();
+
+        Ok(())
     }
 }
