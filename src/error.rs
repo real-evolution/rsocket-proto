cb4fb4fe7694c21b56cab4d5b4936e78a29c117b
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-03 04:40:37 +0300

    ref!: remove no longer used error variants

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,4 @@
-use nom::{error::Error as NomError, Err as NomErr};
 use thiserror::Error;
-
-use crate::frame::Flags;
 
 /// A type alias for [`Result<T, [`Error>`]`].
 pub type Result<T> = std::result::Result<T, Error>;
@@ -9,33 +6,16 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// A type to represent all possible errors that can occur when using this
 /// library.
 #[derive(Debug, Error)]
-    #[error("parsing error (0x{:08X}): {}", nom::error::error_to_u32(.0), .0.description())]
-    Parsing(nom::error::ErrorKind),
-
-    #[error("invalid buffer size: {0}")]
-    BufferLength(&'static str),
-
-    #[error("unexpected frame type: 0x{0:02X}")]
-    UnknownFrameType(u8),
-
-    #[error("invalid stream identifier: expected 0x{expected:08X}, got 0x{actual:08X}")]
-    UnexpectedStreamId { expected: u32, actual: u32 },
-
-    #[error("invalid flags value: 0x{flags:04X}, mask = 0x{mask:04X}")]
-    UnexpectedFlags { flags: Flags, mask: Flags },
 pub enum Error {
+    #[error("encoding/decoding error: {0}")]
+    Codec(#[from] recode::Error),
 
-    #[error("unexpected flag value: {flag:?}, expected = {expected_value}")]
-    UnexpectedFlagValue { flag: Flags, expected_value: bool },
+    #[error("protocol violation: {0}")]
+    ProtocolViolation(&'static str),
 }
 
-impl<I> From<NomErr<NomError<I>>> for RSocketError {
-    fn from(value: NomErr<NomError<I>>) -> Self {
-        match value {
-            | nom::Err::Incomplete(_) => {
-                Self::BufferLength("input buffer too short")
-            }
-            | NomErr::Error(e) | NomErr::Failure(e) => Self::Parsing(e.code),
-        }
+impl From<std::convert::Infallible> for Error {
+    fn from(_: std::convert::Infallible) -> Self {
+        unreachable!()
     }
 }
