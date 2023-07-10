563af98e0e44c2d041cb724084f1893d6b605c59
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-10 04:54:55 +0300

    feat: add type aliases for composed frame codecs

--- src/io/codec/fragmented/mod.rs
+++ src/io/codec/fragmented/mod.rs
@@ -1,5 +1,10 @@
 mod decoder;
 mod encoder;
 
-pub use decoder::FragmentedFrameDecoder;
-pub use encoder::FragmentedFrameEncoder;
+pub use self::decoder::FragmentedFrameDecoder;
+pub use self::encoder::FragmentedFrameEncoder;
+
+pub type FragmentedFrameCodec<const MTU: usize> = super::ComposedCodec<
+    FragmentedFrameDecoder<MTU>,
+    FragmentedFrameEncoder<MTU>,
+>;
--- src/io/codec/mod.rs
+++ src/io/codec/mod.rs
@@ -3,9 +3,16 @@ mod decoder;
 mod encoder;
 mod fragmented;
 
-pub use decoder::FrameDecoder;
-pub use encoder::FrameEncoder;
-pub use fragmented::{FragmentedFrameDecoder, FragmentedFrameEncoder};
 pub use self::composed::ComposedCodec;
+pub use self::decoder::FrameDecoder;
+pub use self::encoder::FrameEncoder;
+pub use self::fragmented::{
+    FragmentedFrameCodec,
+    FragmentedFrameDecoder,
+    FragmentedFrameEncoder,
+};
 
 pub const MAX_FRAME_LEN: usize = 16 * 1024 * 1024;
+
+pub type FrameCodec<const MTU: usize = MAX_FRAME_LEN> =
+    ComposedCodec<FrameDecoder<MTU>, FrameEncoder<MTU>>;
