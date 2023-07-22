aa4e680fc5702dcc39386a07c1c9d70ef3883a16
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-22 03:42:25 +0300

    doc(util): document `AtomicStreamId` type

--- src/util/atomic_stream_id.rs
+++ src/util/atomic_stream_id.rs
@@ -2,12 +2,21 @@ use std::sync::atomic::{AtomicU32, Ordering};
 
 use crate::frame::StreamId;
 
+/// An atomic stream ID generator.
+///
+/// This is a simple atomic counter that generates stream IDs.
+/// It can be used to generate sequenetial (with a step of `STEP`) stream IDs
+/// for both client and server.
 #[derive(Debug)]
 pub struct AtomicStreamId<const STEP: u32 = 2> {
     current: AtomicU32,
 }
 
 impl<const STEP: u32> AtomicStreamId<STEP> {
+    /// Creates a new atomic stream ID generator.
+    ///
+    /// # Parameters
+    /// * `start` - The starting value for the stream ID.
     #[inline]
     pub const fn new(start: u32) -> Self {
         let current = AtomicU32::new(start);
@@ -15,16 +24,23 @@ impl<const STEP: u32> AtomicStreamId<STEP> {
         Self { current }
     }
 
+    /// Creates a new atomic even stream ID generator.
+    ///
+    /// This is equivalent to calling [`Self::new(0)`].
     #[inline]
     pub const fn new_even() -> Self {
         Self::new(0)
     }
 
+    /// Creates a new atomic odd stream ID generator.
+    ///
+    /// This is equivalent to calling [`Self::new(1)`].
     #[inline]
     pub const fn new_odd() -> Self {
         Self::new(1)
     }
 
+    /// Gets the next stream ID in the sequence.
     #[inline]
     pub fn next(&self) -> StreamId {
         let value = self.current.fetch_add(STEP, Ordering::Relaxed);
@@ -32,13 +48,16 @@ impl<const STEP: u32> AtomicStreamId<STEP> {
         StreamId::new(value)
     }
 
+    /// Gets the next stream ID in the sequence without incrementing the
+    /// counter.
     #[inline]
-    pub fn at(&self, steps: u32) -> StreamId {
-        let value = self.current.fetch_add(STEP * steps, Ordering::Relaxed);
+    pub fn peek_next(&self) -> StreamId {
+        let value = self.current.load(Ordering::Relaxed);
 
-        StreamId::new(value)
+        StreamId::new(value + STEP)
     }
 
+    /// Gets the current stream ID in the sequence.
     #[inline]
     pub fn current(&self) -> StreamId {
         let current = self.current.load(Ordering::Relaxed);
